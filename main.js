a939d0cfc0b26b34de9e3e963c3b7efaf621fc65
ehsan4T5 <158603664+ehsan4T5@users.noreply.github.com>
2024-02-20 10:28:35 -0800

    Add files via upload
    mainjs

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
-var hw = "Hello World";
+/* var hw = "Hello World"
+
 console.log(hw);
-var arr = [1, 2, 3, 4, 5];
+
+
+
+var arr = [1,2,3,4,5]
 console.log(arr);
+
+
 var obj = {
     name: "Ehsan",
     course: "Generative Ai"
-};
+}
+
 console.log(obj);
+
+
+
 console.log("as salam o Aliqum");
-console.log(obj.name);
+console.log(obj.name); */
+// =====================================================================================
+// Ex-2
+// var personNamme = "Ehsan";
+// console.log('Hello ' +personNamme+ ' would you like to lern some Python today');
+// =========================================================================================
+// Ex-3 
+var personNamme = 'Ehsan';
+console.log(personNamme.toLowerCase());
+console.log(personNamme.toUpperCase());
+console.log(personNamme);
